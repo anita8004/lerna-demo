30e6863d2a5cb6de7e87ed2010b44545a5539629
anita <anita_li@paradise-soft.com.tw>
2023-12-25 13:46:20 +0800

    add full name

---
+++ packages/main-project/src/components/FullName.vue
@@ -0,0 +1,50 @@
+<script lang="ts">
+export default {
+  name: 'FullName',
+  setup() {
+    const firstName = ref('Danny')
+    const lastName = ref('Li')
+    const getFullName = (firstName: string, lastName: string) => [firstName, lastName].filter(Boolean).join(' ')
+    const fullName = computed({
+      get() {
+        // return firstName.value + ' ' + lastName.value
+        return getFullName(firstName.value, lastName.value)
+      },
+      set(newValue) {
+        [firstName.value = '', lastName.value = ''] = newValue.split(' ')
+        // const [first, last] = newValue.split(' ')
+        // firstName.value = first || ''
+        // lastName.value = last || ''
+      }
+    })
+    return {
+      firstName,
+      lastName,
+      fullName
+    }
+  }
+}
+</script>
+
+<template>
+  <form>
+    <div class="form-item">
+      <label for="">First Name: </label>
+      <input v-model="firstName" />
+    </div>
+    <div class="form-item">
+      <label for="">Last Name: </label>
+      <input v-model="lastName" />
+    </div>
+    <div class="form-item">
+      <label for="">Full Name: </label>
+      <input v-model="fullName" />
+    </div>
+  </form>
+  <ul>
+    <li>first name: {{ firstName }}</li>
+    <li>last name: {{ lastName }}</li>
+  </ul>
+</template>
+
+<style lang="scss" scoped></style>
